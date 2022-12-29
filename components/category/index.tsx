f5c9a8b7f88400d197543bbcbc19b75246b80ee9
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-12-29 22:59:37 +0600

    Product desing done

--- components/category/index.tsx
+++ components/category/index.tsx
@@ -1,29 +1,16 @@
 import React from 'react'
 import { Category } from '../../types/components/category'
 
-const Category: React.FC<Category> = ({name, image}: {
-  name:string,
+const Category: React.FC<Category> = ({ name, image }: {
+  name: string,
   image: string
 }) => {
   return (
     <div>
-     
-           
-
-            <div className='relative overflow-hidden'>
-                <img src={image} className='h-32 w-full' alt="" />
-                <a href="" className='absolute text-xl text-black font-bold bg-opacity-40 inset-0 text-center flex justify-center items-center'>{name}</a>
-            </div>
-
-
-
-
-
-
-        
-
-
-        
+      <div className='relative overflow-hidden'>
+        <img src={image} className='h-32 w-full' alt="" />
+        <a href="" className='absolute text-xl text-black font-bold bg-opacity-40 inset-0 text-center flex justify-center items-center'>{name}</a>
+      </div>
     </div>
   )
 }
