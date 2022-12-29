0ae00476bb456b0eb08bae013b132440d8321656
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-12-30 03:57:21 +0600

    Order page done

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -70,7 +70,52 @@ export default function Order() {
 
           </div>
           <div className="col-span-1">
-            asdfasdf
+            <div className='shadow-lg'>
+              <h2 className='font-bold my-8 px-4'>Your Order</h2>
+              <div className="grid grid-cols-2 py-3">
+                <div className="flex gap-4 px-4">
+                    <div className=''>
+                        <img src="/product1.jpg" className='h-16' alt="" />
+                    </div>
+                    <div>
+                      <span className='font-bold'>Sprite</span><br />
+                      <span className=''><span className='font-bold'>$200</span> &nbsp; x 2</span><br />
+                    </div>
+                </div>
+              </div>
+              <hr />
+              <div className="grid grid-cols-2 py-3">
+                <div className="flex gap-4 px-4">
+                    <div className=''>
+                        <img src="/product1.jpg" className='h-16' alt="" />
+                    </div>
+                    <div>
+                      <span className='font-bold'>Sprite</span><br />
+                      <span className=''><span className='font-bold'>$200</span> &nbsp; x 2</span><br />
+                    </div>
+                </div>
+              </div>
+              <hr />
+              <div className="grid grid-cols-2 py-3">
+                <div className="flex gap-4 px-4">
+                    <div className=''>
+                        <img src="/product1.jpg" className='h-16' alt="" />
+                    </div>
+                    <div>
+                      <span className='font-bold'>Sprite</span><br />
+                      <span className=''><span className='font-bold'>$200</span> &nbsp; x 2</span><br />
+                    </div>
+                </div>
+              </div>
+
+              <div className='flex justify-between  py-4 px-4'>
+                <div className=''>
+                  <span className='font-bold text-2xl'>Total</span>
+                </div>
+                <div><span className='font-bold'>$2323</span></div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
